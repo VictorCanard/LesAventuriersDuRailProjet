1bf54aa9304730873403ce1d7a1878d6b0c71b35
Anne-Marie <annierusu@gmail.com>
2021-02-22 18:13:25 +0100

    Classe Card

--- java/ch/epfl/tchu/game/Card.java
+++ java/ch/epfl/tchu/game/Card.java
@@ -1,26 +1,47 @@
 package ch.epfl.tchu.game;
 
 import java.util.List;
-//hi
 
 public enum Card {
-    BLACK,VIOLET,BLUE,GREEN,YELLOW,ORANGE,RED,WHITE,LOCOMOTIVE;
+    BLACK(Color.BLACK),
+    VIOLET(Color.VIOLET),
+    BLUE(Color.BLUE),
+    GREEN(Color.GREEN),
+    YELLOW(Color.YELLOW),
+    ORANGE(Color.ORANGE),
+    RED(Color.RED),
+    WHITE(Color.WHITE),
+    LOCOMOTIVE(null);
 
-    private Color color = null;
+    private Color color;
+
     public final static List<Card> ALL = List.of(Card.values());
     public final static int COUNT = ALL.size();
-
     public final static List<Card> CARS = ALL.subList(0,8);
 
-    Card(){} //Gros doute ici, fin de la partie 3.4
+    /**
+     * @param color : the color of the corresponding card
+     */
     Card(Color color){
         this.color = color;
     }
 
+    /**
+     * @param color : the color of a card
+     * @return : the type of the card corresponding to the given color
+     */
     public static Card of(Color color){
         return ALL.get(color.ordinal());
     }
+
+    /**
+     * @return : the color of the given card
+     */
     public Color color(){
-        return this.color;
+        if(this.color == null){
+            return null;
+        }else {
+            return this.color;
+        }
     }
 }
