ab75553e83475a31b2491f214972cf373aa92c3f
VictorGitPato <69410903+VictorGitPato@users.noreply.github.com>
2021-03-05 18:22:07 +0100

    -Classe Deck terminée, restent les tests -Une bonne partie de la classe StringsFr remplie

--- src/ch/epfl/tchu/game/Deck.java
+++ src/ch/epfl/tchu/game/Deck.java
@@ -1,26 +1,54 @@
 package ch.epfl.tchu.game;
 
+import ch.epfl.tchu.Preconditions;
 import ch.epfl.tchu.SortedBag;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public final class Deck<C extends Comparable<C>> {
-    private Deck(){
+    private final List<C> DECK_CARDS;
 
+    private Deck(List<C> shuffledCards){
+        DECK_CARDS = shuffledCards;
     }
     public static <C extends Comparable<C>> Deck<C> of(SortedBag<C> cards, Random rng){
+        List<C> listOfCards = cards.toList();
+        Collections.shuffle(listOfCards, rng);
 
+        return new Deck<>(listOfCards);
     }
     public int size(){
-
+        return DECK_CARDS.size();
     }
-    public boolean isEmpty(){}
+    public boolean isEmpty(){
+        return DECK_CARDS.isEmpty();
+    }
 
-    public C topCard(){}
+    public C topCard(){
+        Preconditions.checkArgument(!isEmpty());
+
+        return DECK_CARDS.get(0);
+    }
+
     public Deck<C> withoutTopCard(){
+        Preconditions.checkArgument(!isEmpty());
+
+        return new Deck<>(DECK_CARDS.subList(1, size()));
 
     }
-    public SortedBag<C> topCards(int count){}
+    public SortedBag<C> topCards(int count){
+        Preconditions.checkArgument(0<=count && count <=size());
 
-    public Deck<C> withoutTopCards(int count){}
+        SortedBag<C> topCards = SortedBag.of(DECK_CARDS.subList(0, count));
+        return topCards;
+    }
+
+    public Deck<C> withoutTopCards(int count){
+        Preconditions.checkArgument(0<=count && count <=size());
+
+        return new Deck<>(DECK_CARDS.subList(count,size()));
+    }
 }
--- src/ch/epfl/tchu/gui/Info.java
+++ src/ch/epfl/tchu/gui/Info.java
@@ -2,29 +2,76 @@ package ch.epfl.tchu.gui;
 
 import ch.epfl.tchu.SortedBag;
 import ch.epfl.tchu.game.Card;
+import ch.epfl.tchu.game.Color;
 import ch.epfl.tchu.game.Route;
 import ch.epfl.tchu.game.Trail;
 
 import java.util.List;
 
 public final class Info {
-    public Info(String playerName){}
-    public static String cardName(Card card, int count){}
-            //(utilise BLACK_CARD, BLUE_CARD, etc.),
-    public static String draw(List<String> playerNames, int points){}
-            //(utilise DRAW).
+    private final String playerName;
 
-    public String willPlayFirst(){}
-    public String keptTickets(int count){}
-    public String canPlay(){}
-    public String drewTickets(int count){}
-    public String drewBlindCard(){}
-    public String drewVisibleCard(Card card){}
-    public String claimedRoute(Route route, SortedBag<Card> cards){}
-    public String attemptsTunnelClaim(Route route, SortedBag<Card> initialCards){}
-    public String drewAdditionalCards(SortedBag<Card> drawnCards, int additionalCost){}
+    public Info(String playerName){
+        this.playerName = playerName;
+    }
+    public static String cardName(Card card, int count){
+        Color cardColor = card.color();
+        String cardFrenchName = StringsFr.cardColor;
+        return StringsFr.
+    }
+            //(utilise BLACK_CARD, BLUE_CARD, etc.),
+    public static String draw(List<String> playerNames, int points){
+        String playersMessage = String.format("%s%t%u", playerNames.get(0), StringsFr.AND_SEPARATOR, playerNames.get(1));
+        String drawMessage = String.format(StringsFr.DRAW, playersMessage, points);
+        return drawMessage;
+    }
+
+
+    public String willPlayFirst(){
+        return String.format(StringsFr.WILL_PLAY_FIRST, playerName);
+    }
+    public String keptTickets(int count){
+        return String.format(StringsFr.KEPT_N_TICKETS, playerName, count);
+    }
+    public String canPlay(){
+        return String.format(StringsFr.CAN_PLAY, playerName);
+    }
+    public String drewTickets(int count){
+        return String.format(StringsFr.DREW_TICKETS, playerName, count);
+    }
+    public String drewBlindCard(){
+        return String.format(StringsFr.DREW_BLIND_CARD, playerName);
+    }
+    public String drewVisibleCard(Card card){
+        return String.format(StringsFr.DREW_VISIBLE_CARD, cardName(card, 1));
+    }
+    public String claimedRoute(Route route, SortedBag<Card> cards){
+        return String.format(StringsFr.CLAIMED_ROUTE, playerName, route, cardNames(cards));
+    }
+    public String attemptsTunnelClaim(Route route, SortedBag<Card> initialCards){
+        return String.format(StringsFr.ATTEMPTS_TUNNEL_CLAIM, playerName, route, cardNames(initialCards));
+    }
+    public String drewAdditionalCards(SortedBag<Card> drawnCards, int additionalCost){
+        String additionalCards = String.format(StringsFr.ADDITIONAL_CARDS_ARE, cardNames(drawnCards));
+
+        if()
+        String additionalCost = String.format(StringsFr.ADD);
+    }
     public String didNotClaimRoute(Route route){}
     public String lastTurnBegins(int carCount){}
     public String getsLongestTrailBonus(Trail longestTrail){}
     public String won(int points, int loserPoints){}
+
+    private static String cardNames(SortedBag<Card> bagOfCards){
+        StringBuilder stringOfAllCardNamesToReturn = new StringBuilder();
+        for (Card c: bagOfCards.toSet()) {
+            int n = bagOfCards.countOf(c);
+            stringOfAllCardNamesToReturn
+                    .append(cardName(c, 1)) //Fix the count issue
+                    .append(" ")
+                    .append(n);
+        }
+        return stringOfAllCardNamesToReturn.toString();
+
+    }
 }
