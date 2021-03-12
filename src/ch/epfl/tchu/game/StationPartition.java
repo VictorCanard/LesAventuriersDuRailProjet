7a19672c90811293a11ab5f1ce051af4ea4a0dd2
Anne-Marie <annierusu@gmail.com>
2021-03-12 16:37:03 +0100

    Debut StationPartition

--- src/ch/epfl/tchu/game/StationPartition.java
+++ src/ch/epfl/tchu/game/StationPartition.java
@@ -1,7 +1,47 @@
 package ch.epfl.tchu.game;
 
-public final class StationPartition {
+public final class StationPartition implements StationConnectivity {
+
+    private StationPartition(int[] repLinks) {
+        repLinks = new int[ChMap.stations().size()];
+    }
+
     public final class Builder{
 
+        public Builder(int stationCount){
+
+        }
+
+        public Builder connect(Station s1, Station s2){
+
+        }
+
+        public StationPartition build(){
+
+        }
+        private int representative(int station){
+
+           // return representative id of subset that station is in
+        }
     }
+
+    /**
+     * Verifies if two stations are connected
+     * @param s1 : first train station
+     * @param s2 : second train station
+     * @return : true if they are connected by a (single) players wagons, false otherwise
+     */
+    //Prenez garde au fait que la méthode connected doit également accepter des gares dont l'identité est hors des
+    // bornes du tableau passé à son constructeur. Lorsqu'au moins une des gares qu'on lui passe est ainsi hors bornes,
+    // elle ne retourne vrai que si les deux gares ont la même identité.
+    //table 0-50, the largest id is 50????????
+    @Override
+    public boolean connected(Station s1, Station s2) {
+
+
+
+        return false;
+    }
+
+
 }
