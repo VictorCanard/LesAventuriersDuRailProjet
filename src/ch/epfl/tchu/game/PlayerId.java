827601cb7cd42fa33117d53e28c2d555fd51bfd3
Anne-Marie <annierusu@gmail.com>
2021-03-12 14:09:54 +0100

    PlayerId

--- src/ch/epfl/tchu/game/PlayerId.java
+++ src/ch/epfl/tchu/game/PlayerId.java
@@ -1,4 +1,30 @@
 package ch.epfl.tchu.game;
 
+import java.util.List;
+
 public enum PlayerId {
+    PLAYER_1, PLAYER_2;
+
+    /**
+     * List of all the players
+     */
+    public final static List<PlayerId> ALL = List.of(PlayerId.values());
+
+    /**
+     * Number of players
+     */
+    public final static int COUNT = ALL.size();
+
+    /**
+     *Getter for the next player to have a turn
+     * @return the next player
+     */
+    public PlayerId next(){
+
+        if(this.equals(PLAYER_1)){
+            return PLAYER_2;
+        }else{
+            return PLAYER_1;
+        }
+    }
 }
