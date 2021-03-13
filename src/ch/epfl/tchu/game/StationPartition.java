777319a9acd623883ed4885dd0ba56dec96db0a6
Anne-Marie <annierusu@gmail.com>
2021-03-13 19:03:26 +0100

    un peu plus sur StationPartition 2

--- src/ch/epfl/tchu/game/StationPartition.java
+++ src/ch/epfl/tchu/game/StationPartition.java
@@ -7,6 +7,7 @@ import java.util.stream.IntStream;
 public final class StationPartition implements StationConnectivity {
 
     private final int[] PARTITIONS;
+    private final int STATION_COUNT;
 
 
     private StationPartition(int[] repLinks) { //array already with the representatives in the entries (idk if flattened or not)
@@ -14,16 +15,25 @@ public final class StationPartition implements StationConnectivity {
         for(int i= 0; i< repLinks.length; i++){
             PARTITIONS[i] = repLinks[i];
         }
+
+        STATION_COUNT = Builder.theStationCount;
     }
 
-    public final class Builder{
+
+    public static final class Builder{
+
+        private static int theStationCount;
 
         public Builder(int stationCount){ //argument is what was calculated in PlayerState ticketPoints. ex if max station id is 25, gives 26
             Preconditions.checkArgument(stationCount>=0);
-
+            theStationCount = stationCount;
             int [] idArray = IntStream.range(0, stationCount).toArray(); //ex now you have a table from 0 to 25 because 26 excluded
+
+
             //but what if you have stations 0, 24, 25... what about all the other stations and their representatives?
+
         }
+
 
         public Builder connect(Station s1, Station s2){ //je ne comprends pas comment choisir "aleatoirement" le representant, cest nous qui choisisons ou cest Random
            int id1 = s1.id();
@@ -31,6 +41,7 @@ public final class StationPartition implements StationConnectivity {
 
            //connect one representative to the other (which has to automatically change the representative of all the elements of the subset)
 
+            return this;
         }
 
         public StationPartition build(){
@@ -41,6 +52,8 @@ public final class StationPartition implements StationConnectivity {
         }
         private int representative(int stationId){
         //manipulates the deep representation apparently
+
+
            return
         }
     }
@@ -54,22 +67,20 @@ public final class StationPartition implements StationConnectivity {
     //Prenez garde au fait que la méthode connected doit également accepter des gares dont l'identité est hors des
     // bornes du tableau passé à son constructeur. Lorsqu'au moins une des gares qu'on lui passe est ainsi hors bornes,
     // elle ne retourne vrai que si les deux gares ont la même identité.
-    //table 0-50, the largest id is 50????????
+
     @Override
     public boolean connected(Station s1, Station s2) {
         int id1 = s1.id();
         int id2 = s2.id();
 
-        if(/*id 1 hors tableau ou id2 hors tableau*/){
-            if(id1 ==id2){
+        if(id1> STATION_COUNT || id2>STATION_COUNT) {
+            if (id1 == id2) {
                 return true; //meaning the stations are the same
             }
-            return false;
-        }else{
-            //if representative of id1 and id2 are the same return true
         }
-
+        if(PARTITIONS[id1] == PARTITIONS[id2]){
+                return true;
+        }
+        return false;
     }
-
-
 }
