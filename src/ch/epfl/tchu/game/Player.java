c72b1097ee4e219cdf659f7559d8744f4c31a653
Anne-Marie <annierusu@gmail.com>
2021-03-19 12:12:38 +0100

    Player method declarations

--- src/ch/epfl/tchu/game/Player.java
+++ src/ch/epfl/tchu/game/Player.java
@@ -1,7 +1,23 @@
 package ch.epfl.tchu.game;
 
+import ch.epfl.tchu.SortedBag;
+
+import java.util.List;
+import java.util.Map;
+
 public interface Player {
-    public static class TurnKind{
+    public static enum TurnKind{
 
     }
+    void initPlayers(PlayerId ownID, Map<PlayerId, String> playerNames);
+    void receiveInfo(String info);
+    void updateState(PublicGameState newState, PlayerState ownState);
+    void setInitialTicketChoice(SortedBag<Ticket> tickets);
+    SortedBag<Ticket> chooseInitialTickets();
+    TurnKind nextTurn();
+    SortedBag<Ticket> chooseTickets(SortedBag<Ticket> options);
+    int drawSlot();
+    Route claimedRoute();
+    SortedBag<Card> initialClaimCards();
+    SortedBag<Card> chooseAdditionalCards(List<SortedBag<Card>> options);
 }
