94210c378db00b740afcd769e9f7f5663ed46044
Anne-Marie <annierusu@gmail.com>
2021-03-19 12:17:02 +0100

    Player

--- src/ch/epfl/tchu/game/Player.java
+++ src/ch/epfl/tchu/game/Player.java
@@ -7,6 +7,14 @@ import java.util.Map;
 
 public interface Player {
     public static enum TurnKind{
+        DRAW_TICKETS,
+        DRAW_CARDS,
+        CLAIM_ROUTE;
+
+        /**
+         * List of all the actions a player can do on their turn
+         */
+        public final static List<TurnKind> ALL = List.of(TurnKind.values());
 
     }
     void initPlayers(PlayerId ownID, Map<PlayerId, String> playerNames);
