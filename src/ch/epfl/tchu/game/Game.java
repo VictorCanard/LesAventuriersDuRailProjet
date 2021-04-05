268a7f8915cfc0ffc2cb0b5b721c2809c1df882c
Anne-Marie <annierusu@gmail.com>
2021-04-05 09:43:10 +0200

    defausse

--- src/ch/epfl/tchu/game/Game.java
+++ src/ch/epfl/tchu/game/Game.java
@@ -160,7 +160,6 @@ public final class Game {
                 Route claimedRoute = player.claimedRoute();
                 SortedBag<Card>  initialClaimCards = player.initialClaimCards();
 
-
                 if(claimedRoute.level() == Level.UNDERGROUND) {
                     receiveInfoForAll(players, infoGenerators.get(currentPlayerId).attemptsTunnelClaim(claimedRoute, initialClaimCards));
 
@@ -188,26 +187,27 @@ public final class Game {
                        possibleAdditionalCards = playerState.possibleAdditionalCards(additionalCost, initialClaimCards, drawnCards);
 
                         if(possibleAdditionalCards.isEmpty()){ //Player can't play any additional cards
+                            gameState = gameState.withMoreDiscardedCards(drawnCards);
                             receiveInfoForAll(players, infoGenerators.get(currentPlayerId).didNotClaimRoute(claimedRoute));
 
                         }else{ //The player can play additional cards. Asks the player which set of cards he want to play.
                             SortedBag<Card> tunnelCards = player.chooseAdditionalCards(possibleAdditionalCards);
+                            gameState = gameState.withMoreDiscardedCards(drawnCards);
+            //no            gameState = putInDiscard(gameState, initialClaimCards.union(tunnelCards).union(drawnCards)); //Cards the player played, drawn cards and the additional cards he played
+                            gameState = gameState.withClaimedRoute(claimedRoute, initialClaimCards.union(tunnelCards)); //Claimed route
 
-                            gameState = putInDiscard(gameState, initialClaimCards.union(tunnelCards).union(drawnCards)); //Cards the player played, drawn cards and the additional cards he played
-                            gameState = gameState.withClaimedRoute(claimedRoute, tunnelCards); //Claimed route
-
-                            receiveInfoForAll(players, infoGenerators.get(currentPlayerId).claimedRoute(claimedRoute, tunnelCards));
+                            receiveInfoForAll(players, infoGenerators.get(currentPlayerId).claimedRoute(claimedRoute, initialClaimCards.union(tunnelCards)));
                         }
                     }
                     else{ //No additional cost
                         gameState = gameState.withClaimedRoute(claimedRoute, initialClaimCards);
-                        gameState = putInDiscard(gameState, initialClaimCards);
+                        gameState = putInDiscard(gameState, drawnCards);
                         receiveInfoForAll(players, infoGenerators.get(currentPlayerId).claimedRoute(claimedRoute, initialClaimCards));
                     }
 
                 }else{
                     gameState = gameState.withClaimedRoute(claimedRoute, initialClaimCards);
-                    gameState = putInDiscard(gameState, initialClaimCards);
+            //no        gameState = putInDiscard(gameState, initialClaimCards);
                     receiveInfoForAll(players, infoGenerators.get(currentPlayerId).claimedRoute(claimedRoute, initialClaimCards));
                 }
 
