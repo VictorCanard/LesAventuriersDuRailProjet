72584000aad6db0814902c482bc4bdd60fa21bf0
VictorGitPato <69410903+VictorGitPato@users.noreply.github.com>
2021-04-17 09:16:36 +0200

    -J'ai fait une partie des attributs

--- src/ch/epfl/tchu/net/Serdes.java
+++ src/ch/epfl/tchu/net/Serdes.java
@@ -1,12 +1,14 @@
 package ch.epfl.tchu.net;
 
 import ch.epfl.tchu.SortedBag;
-import ch.epfl.tchu.game.Color;
+import ch.epfl.tchu.game.*;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
+import java.util.StringJoiner;
+import java.util.regex.Pattern;
 
 
 public class Serdes {
@@ -21,6 +23,38 @@ public class Serdes {
                     Arrays.toString(Base64.getDecoder().decode(serializedString)).getBytes(StandardCharsets.UTF_8),
                     StandardCharsets.UTF_8)
     );
+
+
+//    public static final Serde<String> STRING_SERDE = Serde.of;
+//    public static final Serde<String> STRING_SERDE = Serde.of;
+
+      public static final Serde<Card> CARD_SERDE = Serde.oneOf(List.of(Card.values()));
+
+//    public static final Serde<String> STRING_SERDE = Serde.of;
+//    public static final Serde<String> STRING_SERDE = Serde.of;
+
+      public static final Serde<List<String>> LIST_STRING_SERDE = Serde.listOf(STRING_SERDE, ",");
+    public static final Serde<List<Card>> LIST_CARD_SERDE = Serde.listOf(CARD_SERDE, ",");
+    public static final Serde<List<Route>> LIST_ROUTE_SERDE = Serde.listOf(ROUTE_SERDE, ",");
+
+    public static final Serde<SortedBag<Card>> LIST_STRING_SERDE = Serde.bagOf(CARD_SERDE, ",");
+    public static final Serde<SortedBag<Ticket>> LIST_CARD_SERDE = Serde.bagOf(TICKET_SERDE, ",");
+    public static final Serde<List<SortedBag<Card>>> LIST_ROUTE_SERDE = Serde.listOf(LIST_STRING_SERDE, ";");
+
+//    public static final Serde<String> STRING_SERDE = Serde.of;
+    //    public static final Serde<String> STRING_SERDE = Serde.of;
+//    public static final Serde<String> STRING_SERDE = Serde.of;
+
+
+    public static final Serde<PublicCardState> CARD_STATE_SERDE = Serde.of(
+            (publicCardState) -> new StringJoiner(";").add(LIST_CARD_SERDE.serialize(publicCardState.faceUpCards())
+                                +INTEGER_SERDE.serialize(publicCardState.deckSize())
+                                +INTEGER_SERDE.serialize(publicCardState.discardsSize())).toString(),
+            (string) -> string.split(Pattern.quote(";"), -1).
+    ); //Not sure on how to go from string to the public card's state attributes here
+
+    public static final Serde<PublicGameState> GAME_STATE_SERDE = Serde.;
+
 
 
 //    Serde<Color> color = Serde.oneOf(Color.ALL);
