af44f2de8730cc68c96f27e731abc05cf285ace7
VictorGitPato <69410903+VictorGitPato@users.noreply.github.com>
2021-04-27 18:32:48 +0200

    -Juste les 3 premieres classes de l'E9 ainsi que l'importation pour comprendre

---
+++ src/ch/epfl/tchu/gui/ActionHandlers.java
@@ -0,0 +1,21 @@
+package ch.epfl.tchu.gui;
+
+public interface ActionHandlers {
+    interface DrawTicketsHandler {
+
+    }
+
+    interface DrawCardHandler {
+
+    }
+
+    interface ClaimRouteHandler {
+
+    }
+
+    interface ChooseTicketsHandler {
+    }
+
+    interface ChooseCardsHandler {
+    }
+}
---
+++ src/ch/epfl/tchu/gui/ObservableGameState.java
@@ -0,0 +1,18 @@
+package ch.epfl.tchu.gui;
+
+import ch.epfl.tchu.game.PlayerId;
+
+public final class ObservableGameState {
+    public ObservableGameState(PlayerId player1) {
+    }
+
+    //Group 1
+    //Group 2
+    //Group 3
+
+
+    public boolean canDrawTickets() {
+        return false;
+    }
+
+}
